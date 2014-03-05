a317e2b7b4ce5a3002c56cd520f605195100195f
heyunlong565 <734274314@qq.com>
2014-03-05 14:10:00 +0800

    Update lesson.js

--- lesson.js
+++ lesson.js
@@ -7,3 +7,21 @@ function count(){
 }
 var t = count();
 t();
+
+//oop
+//构造函数法
+function Person(name, age){
+	this.name = name;
+	this.age = age;
+}
+Person.prototype.sayHello = function() {
+	console.log("My name is "+ this.name + " Age is " + this.age);
+};
+Person.prototype.sayHi = function() {
+	console.log("Hi, What is your name ?");
+};
+var person1 = new Person("zhangsan", 20);
+var person2 = new Person("Lisi", 22);
+
+person1.sayHello();
+person2.sayHello();
